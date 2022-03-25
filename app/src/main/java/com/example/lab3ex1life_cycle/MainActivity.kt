0acc9c7721ebedbb4e83361215fe119cd19c99b6
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-03-25 09:51:15 +0500

    life-cycle methods implemented

--- app/src/main/java/com/example/lab3ex1life_cycle/MainActivity.kt
+++ app/src/main/java/com/example/lab3ex1life_cycle/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.lab3ex1life_cycle
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("t0","onCreate method called")
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("t2","onStart method called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("t3","onResume method called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("t4","onPause method called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("t5","onStop method called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("t6","onRestart method called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("t7","onDestroy method called")
     }
 }
